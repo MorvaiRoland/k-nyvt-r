8af9f57181283a8acaa8209bcbb881dc6e20c64d
MorvaiRoland <71487395+MorvaiRoland@users.noreply.github.com>
2021-03-17 20:36:36 +0100

    Kész

---
+++ könyvtár/könyvtár/mentes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace könyvtár
+{
+    class mentes
+    {
+        public static void savedata(object obj, string filename)
+        {
+            XmlSerializer sr = new XmlSerializer(obj.GetType());
+            TextWriter writer = new StreamWriter(filename);
+            sr.Serialize(writer, obj);
+            writer.Close();
+
+
+        }
+    }
+}
